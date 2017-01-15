9fe8898f3d5bd7472cb3ce7b15b22611651b424c
dukemiller <dukemiller928@hotmail.com>
2017-01-15 03:12:31 -0700

    Add basic models

---
+++ school-assignment-reminders/Models/Assignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using GalaSoft.MvvmLight;
+
+namespace school_assignment_reminders.Models
+{
+    [Serializable]
+    public class Assignment: ObservableObject
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("notes")]
+        public string Notes { get; set; }
+
+        [XmlAttribute("position")]
+        public int Position { get; set; }
+
+        [XmlAttribute("completed")]
+        public bool Completed { get; set; }
+
+        [XmlElement("duedate")]
+        public DueDate DueDate { get; set; }
+    }
+}
---
+++ school-assignment-reminders/Models/Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Xml.Serialization;
+using GalaSoft.MvvmLight;
+
+namespace school_assignment_reminders.Models
+{
+    [Serializable]
+    public class Class: ObservableObject
+    {
+        /// <summary>
+        ///     The title, e.g. SER 423
+        /// </summary>
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        ///     The name, e.g. "Mobile Systems"
+        /// </summary>
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     The combined title and fullname, e.g. SER 423 - Mobile Systems
+        /// </summary>
+        [XmlIgnore]
+        public string FullName => $"{Title} - {Name}";
+
+        [XmlElement("assignment")]
+        public List<Assignment> Assignments { get; set; } = new List<Assignment>();
+    }
+}
