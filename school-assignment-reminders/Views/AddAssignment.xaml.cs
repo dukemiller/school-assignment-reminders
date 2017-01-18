2176b48105234160367c4a81533ae654f4033dfc
dukemiller <dukemiller928@hotmail.com>
2017-01-18 11:33:00 -0700

    Add popup dialog views

---
+++ school-assignment-reminders/Views/AddAssignment.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace school_assignment_reminders.Views
+{
+    /// <summary>
+    /// Interaction logic for AddAssignment.xaml
+    /// </summary>
+    public partial class AddAssignment : UserControl
+    {
+        public AddAssignment()
+        {
+            InitializeComponent();
+        }
+    }
+}
